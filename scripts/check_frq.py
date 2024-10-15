e4456e07a8007c101ba62d8a97fdac18d0088c71
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2024-10-15 18:16:36 +0900

    Add data reader Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

---
+++ scripts/check_frq.py
@@ -0,0 +1,23 @@
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+
+class MessageFrequencyChecker:
+    def __init__(self):
+        self.message_count = 0
+        self.last_msg_time = 0
+        rospy.Subscriber('/mocap_pose_topic/frisbee1_pose', PoseStamped, self.callback, queue_size=50)
+
+    def callback(self, msg:PoseStamped):
+        current_time = msg.header.stamp.to_sec()
+        frq = 1.0 / (current_time - self.last_msg_time)
+        self.last_msg_time = current_time
+
+        if frq < 100:
+            print(f"Frequency: {frq} Hz")
+
+
+if __name__ == "__main__":
+    rospy.init_node("message_frequency_checker", anonymous=True)
+    MessageFrequencyChecker()
+    rospy.spin()
