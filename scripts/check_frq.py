c057a3e0f6eee16d789369a31c1ec0baae89b475
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2024-10-16 16:24:30 +0900

    Modify check frq code
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- scripts/check_frq.py
+++ scripts/check_frq.py
@@ -6,15 +6,20 @@ class MessageFrequencyChecker:
     def __init__(self):
         self.message_count = 0
         self.last_msg_time = 0
-        rospy.Subscriber('/mocap_pose_topic/frisbee1_pose', PoseStamped, self.callback, queue_size=50)
+        rospy.Subscriber('/mocap_pose_topic/frisbee2_pose', PoseStamped, self.callback, queue_size=50)
 
     def callback(self, msg:PoseStamped):
         current_time = msg.header.stamp.to_sec()
         frq = 1.0 / (current_time - self.last_msg_time)
         self.last_msg_time = current_time
 
+        if frq < 120 and frq > 110:
+            rospy.loginfo(f"Frequency: {frq} Hz")
+        if frq < 110 and frq > 100:
+            # roswarn
+            rospy.logwarn(f"Frequency: {frq} Hz")
         if frq < 100:
-            print(f"Frequency: {frq} Hz")
+            rospy.logerr(f"Frequency: {frq} Hz")
 
 
 if __name__ == "__main__":
