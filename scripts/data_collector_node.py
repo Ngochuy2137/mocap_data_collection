ccdc202b55ec901de23feab4504f970588f5c231
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2024-10-16 20:21:24 +0900

    Start checking and collecting data when object crosses specific x, y values
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- scripts/data_collector_node.py
+++ scripts/data_collector_node.py
@@ -49,16 +49,31 @@ import threading
 import os
 
 class RoCatDataCollector:
-
-    def __init__(self, mocap_object_topic, min_height_threshold, min_freq_threshold_warn, min_freq_threshold_err, swap_yz):
+    def __init__(self, mocap_object_topic,
+                swap_yz,
+                stop_traj_z_threshold,
+                low_freq_level1_threshold, 
+                low_freq_level2_threshold, 
+                low_freq_l2_num_threshold, 
+                low_freq_x_start_check, 
+                low_freq_y_start_check, 
+                low_freq_z_start_check):
+        
         self.decima_prt_num = 5
         self.start_time = time.strftime("%d-%m-%Y_%H-%M-%S")     # get time now: d/m/y/h/m
         self.number_subscriber = rospy.Subscriber(mocap_object_topic, PoseStamped, self.pose_callback, queue_size=1000)
         self.mocap_object_topic = mocap_object_topic
 
-        self.min_height_threshold = min_height_threshold
-        self.min_freq_threshold_warn = min_freq_threshold_warn
-        self.min_freq_threshold_err = min_freq_threshold_err
+        self.stop_traj_z_threshold, = stop_traj_z_threshold,
+
+        self.low_freq_level1_threshold = low_freq_level1_threshold
+        self.low_freq_level2_threshold = low_freq_level2_threshold
+        # Conditions for apply low frequency level 2 treatment
+        self.low_freq_l2_num_threshold = low_freq_l2_num_threshold
+        self.low_freq_x_start_check = low_freq_x_start_check
+        self.low_freq_y_start_check = low_freq_y_start_check
+        self.low_freq_z_start_check = low_freq_z_start_check
+
         self.swap_yz = swap_yz
         self.collected_data = []
         self.reset()
@@ -103,11 +118,16 @@ class RoCatDataCollector:
         Without finally, when an error (or exception) occurs, the lock can be held forever, and no other thread can obtain the lock.
         '''
         try:
-            if self.recording:    
+            enable_recording = (self.recording and 
+                                self.current_position[0] > self.low_freq_x_start_check and
+                                self.current_position[1] > self.low_freq_y_start_check)
+            if enable_recording:    
                 # check if messages frequency is too low
-                if self.is_message_low_frequency(current_timestamp) == 'err' and self.too_low_freq_count >= 2:
+                low_freq_level = self.is_message_low_frequency(current_timestamp, self.current_position)
+                if low_freq_level == 2:
                     self.enter_event.set()  # Kích hoạt thread để kiểm tra ENTER
                     self.reset()
+                    rospy.logerr("Please recollect the trajectory !")
                     return          
                   
                 # Save the current position to the current trajectory
@@ -116,10 +136,10 @@ class RoCatDataCollector:
 
                 # print out
                 new_point_prt = [round(i, self.decima_prt_num) for i in new_point]
-                print('     New point: x=', new_point_prt[0], ' y=', new_point_prt[1], ' z=', new_point_prt[2], ' t=', new_point_prt[3], ' low_freq_count=', self.low_freq_count)
+                print('     New point: x=', new_point_prt[0], ' y=', new_point_prt[1], ' z=', new_point_prt[2], ' t=', new_point_prt[3], ' low_freq_l1_count=', self.low_freq_l1_count)
 
                 # Check end condition of the current trajectory
-                if self.current_position[2] < self.min_height_threshold:
+                if self.current_position[2] < self.stop_traj_z_threshold:
                     rospy.loginfo("Trajectory ended with " + str(len(self.current_trajectory)) + " points !")
                     if self.process_trajectory(self.current_trajectory):
                         # Save all trajectories to file after each new proper trajectory
@@ -130,7 +150,7 @@ class RoCatDataCollector:
                 # measure callback function time
                 end_time = time.time()
                 callback_freq = 1.0 / (end_time - start_time)
-                if callback_freq < self.min_freq_threshold_err:
+                if callback_freq < self.low_freq_level2_threshold:
                     rospy.logerr("     Callback freq: " + str(round(callback_freq, 2)) + " Hz")
 
         finally:
@@ -140,8 +160,8 @@ class RoCatDataCollector:
     def reset(self,):
         self.current_position = [0, 0, 0]
         self.current_trajectory = []
-        self.low_freq_count = 0
-        self.too_low_freq_count = 0
+        self.low_freq_l1_count = 0
+        self.low_freq_l2_count = 0
         self.recording = False
         self.last_msg_id = 0
         self.last_timestamp = 0.0
@@ -173,32 +193,40 @@ class RoCatDataCollector:
     '''
     Check if messages frequency is too low
     return:
-        False if frequency is fast enough
-        True if frequency is too low
+        0 if frequency is fast enough
+        1 if frequency is in range of low_freq_level1_threshold and low_freq_level2_threshold
+        2 if frequency is lower than low_freq_level2_threshold
     '''
-    def is_message_low_frequency(self, current_timestamp):
-        if self.last_timestamp == 0:
+    def is_message_low_frequency(self, current_timestamp, current_position):
+        enable_low_freq_check = (self.last_timestamp != 0 and
+                                 current_position[0] > self.low_freq_x_start_check and
+                                 current_position[1] > self.low_freq_y_start_check and
+                                 current_position[2] > self.low_freq_z_start_check)
+        if not enable_low_freq_check:
             self.last_timestamp = current_timestamp
-            return 'no'
-        if self.last_timestamp != 0:
-            time_diff = (current_timestamp - self.last_timestamp)
-            freq = 1.0 / time_diff
-            if freq >= self.min_freq_threshold_warn:
-                self.last_timestamp = current_timestamp
-                return 'no'
-            if freq < self.min_freq_threshold_warn and freq >= self.min_freq_threshold_err:
-                freq = round(freq, 3)
-                rospy.logwarn("Current message frequency is in warning level (100-110 hz): " + str(freq) + " Hz")
-                self.last_timestamp = current_timestamp
-                self.low_freq_count += 1
-                return 'warn'
-            if freq < self.min_freq_threshold_err:
+            return 0
+        
+        time_diff = (current_timestamp - self.last_timestamp)
+        freq = 1.0 / time_diff
+        if freq >= self.low_freq_level1_threshold:
+            self.last_timestamp = current_timestamp
+            return 0
+        if freq < self.low_freq_level1_threshold and freq >= self.low_freq_level2_threshold:
+            freq = round(freq, 3)
+            rospy.logwarn("Current message frequency is in warning level (100-110 hz): " + str(freq) + " Hz")
+            self.last_timestamp = current_timestamp
+            self.low_freq_l1_count += 1
+            return 1
+        if freq < self.low_freq_level2_threshold:
+            if  self.low_freq_l2_count >= self.low_freq_l2_num_threshold:
                 freq = round(freq, 3)
                 rospy.logerr("Current message frequency is in err level (<100 hz): " + str(freq) + " Hz")
                 self.last_timestamp = current_timestamp
-                self.low_freq_count += 1
-                self.too_low_freq_count += 1
-                return 'err'
+                self.low_freq_l1_count += 1
+                self.low_freq_l2_count += 1
+                rospy.logerr("The current trajectory is rejected due to low frequency level 2")
+                return 2
+            return 1
     
     '''
     Check if data is uncontinuous
@@ -238,12 +266,11 @@ class RoCatDataCollector:
             print('zeros shape: ', zeros.shape)
             print('gravity shape: ', gravity.shape)
             extened_data_points = np.column_stack((new_traj_np[:, :3], vx, vy, vz, zeros, zeros, gravity))
-            # extened_data_points = np.round(extened_data_points, DECIMAL_NUM)
             trajectory_data = {
                 'points': extened_data_points,
                 'msg_ids': msg_ids,
                 'time_stamps': time_stamps,
-                'low_freq_num': self.low_freq_count
+                'low_freq_num': self.low_freq_l1_count
             }
             self.collected_data.append(trajectory_data)
             return True
@@ -300,16 +327,33 @@ class RoCatDataCollector:
 
 if __name__ == '__main__':
     rospy.init_node('data_collector', anonymous=True)
-    # Thresholds
-    MIN_HEIGHT_THRESHOLD = 0.1  # Trajectory end height threshold
-    MIN_FREQ_THRESHOLD_WARN = 110  # Minimum message frequency (Hz)
-    MIN_FREQ_THRESHOLD_ERR = 100  # Minimum message frequency (Hz)
-
     MOCAP_OBJECT_TOPIC = '/mocap_pose_topic/frisbee2_pose'
-    SWAP_YZ = True
-    DECIMAL_NUM = 5
+    # Variables for stop collecting current trajectory
+    STOP_TRAJ_Z_THRESHOLD = 0.1  # Trajectory end height threshold
 
-    collector = RoCatDataCollector(MOCAP_OBJECT_TOPIC, MIN_HEIGHT_THRESHOLD, MIN_FREQ_THRESHOLD_WARN, MIN_FREQ_THRESHOLD_ERR, SWAP_YZ)
+    # Variables for checking message frequency
+    LOW_FREQ_LEVEL1_THRESHOLD = 110  # Minimum message frequency (Hz)
+    LOW_FREQ_LEVEL2_THRESHOLD = 100  # Minimum message frequency (Hz)
+    LOW_FREQ_L2_NUM_THRESHOLD = 3     # Number of times the message frequency is too low before applying low frequency level 2 treatment: reject current trajectory, reset to collect a new trajectory
+    # Condition to activate the low frequency check 
+    low_freq_check_conditions = {
+        'low_freq_x_start_check': 0.1,     # Start to check when x > low_freq_x_start_check
+        'low_freq_y_start_check': 0.1,     # Start to check when y > low_freq_y_start_check
+        'low_freq_z_start_check': 0.1      # Start to check when z > low_freq_z_start_check
+    }
+    SWAP_YZ = True
+
+    collector = RoCatDataCollector(
+        MOCAP_OBJECT_TOPIC, 
+        SWAP_YZ, 
+        STOP_TRAJ_Z_THRESHOLD, 
+        LOW_FREQ_LEVEL1_THRESHOLD, 
+        LOW_FREQ_LEVEL2_THRESHOLD, 
+        LOW_FREQ_L2_NUM_THRESHOLD,
+        low_freq_check_conditions['low_freq_x_start_check'], 
+        low_freq_check_conditions['low_freq_y_start_check'], 
+        low_freq_check_conditions['low_freq_z_start_check']
+    )
     try:
         rospy.spin()
     except rospy.ROSInterruptException:
