69c7860b917a591d68c4a7c6f81e48da54837884
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2024-10-23 23:24:26 +0900

    Update check frq
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- scripts/check_frq.py
+++ scripts/check_frq.py
@@ -6,14 +6,22 @@ class MessageFrequencyChecker:
     def __init__(self):
         self.message_count = 0
         self.last_msg_time = 0
+        self.last_id = 0
         rospy.Subscriber('/mocap_pose_topic/frisbee1_pose', PoseStamped, self.callback, queue_size=50)
 
     def callback(self, msg:PoseStamped):
         current_time = msg.header.stamp.to_sec()
+        current_id = msg.header.seq
+
+        # print('\n -------')
+        # print('check 111: ', current_time, ' ', self.last_msg_time)
+        # print('check 222: ', current_id, ' ', self.last_id)
+
         frq = 1.0 / (current_time - self.last_msg_time)
         self.last_msg_time = current_time
+        self.last_id = current_id
 
-        if frq < 120 and frq > 110:
+        if frq > 110:
             rospy.loginfo(f"Frequency: {frq} Hz")
         if frq < 110 and frq > 100:
             # roswarn
